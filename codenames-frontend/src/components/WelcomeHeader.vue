63f6007de517f0d9a92f9e7b4c697f76e4077cc7
Hudák Elza <hudak.elza@gmail.com>
2020-05-14 13:07:49 +0200

    fix some design

--- codenames-frontend/src/components/WelcomeHeader.vue
+++ codenames-frontend/src/components/WelcomeHeader.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="main-div col-sm-12">
         <div class="codenames-header">
-            <b-img :src="logoUrl" ></b-img>
+            <b-img :src="logoUrl"></b-img>
         </div>
         <div class="text-center">
             <b-button squared @click="createLobby" class="mr-5">
@@ -25,28 +25,44 @@
 
         public createLobby() {
             this.$store.dispatch("createLobby");
-           // router.push({name: "Lobby", params: {lobbyId: lobbyModel.id}});
+            // router.push({name: "Lobby", params: {lobbyId: lobbyModel.id}});
         }
     }
 </script>
 
 <style scoped>
-
-    button {
-        background-color: rgb(135, 25, 75);
-        border: none;
+    .main-div {
+        background-image: url("../assets/background.svg");
+        background-repeat: no-repeat;
+        background-size: 100%;
+        width: 100vw;
+        height: 100vh;
     }
 
+
     img {
-        max-width: 100%;
+        max-width: 75%;
         margin-bottom: 15vh;
     }
 
-    /*fix active grey */
-    button:hover,button:active,button:focus {
-        outline: none;
-        box-shadow: none;
-        background-color: rgb(180, 65, 75);
+    button {
+        background-color: rgb(135, 25, 75);
+        border: 0 solid;
+        box-shadow: inset 0 0 20px rgba(250, 230, 15, 0);
+        outline: 1px solid;
+        outline-color: rgba(135, 25, 75, .5);
+        outline-offset: 0px;
+        text-shadow: none;
+        transition: all 1250ms cubic-bezier(0.19, 1, 0.22, 1);
+    }
+
+    button:hover {
+        background-color: rgb(135, 25, 75);
+        border: 0px solid;
+        box-shadow: inset 0 0 20px rgba(250, 230, 15, .5), 0 0 20px rgba(250, 230, 15, .2);
+        outline-color: rgba(250, 230, 15, 0);
+        outline-offset: 15px;
+        text-shadow: 1px 1px 2px #427388;
     }
 
 </style>
