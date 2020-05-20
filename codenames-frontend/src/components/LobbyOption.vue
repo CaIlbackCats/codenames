9b9595fc51601834acb2d2632384f24dc39497f0
Hudák Elza <hudak.elza@gmail.com>
2020-05-20 20:08:56 +0200

    fix buttons & tooltips

--- codenames-frontend/src/components/LobbyOption.vue
+++ codenames-frontend/src/components/LobbyOption.vue
@@ -1,10 +1,11 @@
 <template>
-    <div>
-        <b-button size="sm" block squared @click="randomizeRoles" :disabled="partySize<MIN_PARTY_SIZE">Random role
-        </b-button>
-        <b-button size="sm" block squared @click="randomizeSide" :disabled="partySize<MIN_PARTY_SIZE">Random side</b-button>
-        <b-button size="sm" block squared :disabled="!getEveryOneRdy">Start The Game!</b-button>
-        <span v-if="partySize<MIN_PARTY_SIZE">There must be at least 4 players to use these functions</span>
+    <div class="align-center">
+        <b-button :title="partySize<MIN_PARTY_SIZE ? 'There must be at least 4 players to use this function': '' "
+                  size="sm" block squared @click="randomizeRoles" :disabled="partySize<MIN_PARTY_SIZE">Random role</b-button>
+        <b-button :title="partySize<MIN_PARTY_SIZE ? 'There must be at least 4 players to use this function': '' "
+                  size="sm" block squared @click="randomizeSide" :disabled="partySize<MIN_PARTY_SIZE">Random side</b-button>
+        <b-button :title="getEveryOneRdy ? '' : '' "
+                size="md" block squared :disabled="!getEveryOneRdy" class="mt-3">Start The Game!</b-button>
     </div>
 </template>
 
@@ -45,6 +46,11 @@
 </script>
 
 <style scoped>
+    small{
+        color: rgb(135, 25, 75);
+        opacity: 0.6;
+    }
+
     button {
         background-color: rgb(135, 25, 75);
         border: 0 solid;
@@ -56,7 +62,7 @@
         transition: all 1250ms cubic-bezier(0.19, 1, 0.22, 1);
     }
 
-    button:hover {
+    button:enabled:hover {
         background-color: rgb(135, 25, 75);
         border: 0px solid;
         box-shadow: inset 0 0 20px rgba(250, 230, 15, .5), 0 0 20px rgba(250, 230, 15, .2);
