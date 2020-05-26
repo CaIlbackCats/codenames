4de395e38ffcb4cade3225fe345d5940779ec7ae
laura <tundelaura93@gmail.com>
2020-05-26 13:05:41 +0100

    add words to db script and refactor config for deployment

--- src/main/java/com/callbackcats/codenames/game/domain/Word.java
+++ src/main/java/com/callbackcats/codenames/game/domain/Word.java
@@ -1,11 +1,9 @@
 package com.callbackcats.codenames.game.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "word")
 public class Word {
 
     @Id
