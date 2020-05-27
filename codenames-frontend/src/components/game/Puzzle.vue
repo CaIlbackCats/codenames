b86b89593642f5860130803b24b3c8a64970750e
Hudák Elza <hudak.elza@gmail.com>
2020-05-27 12:29:03 +0200

    add puzzle component

--- codenames-frontend/src/components/game/Puzzle.vue
+++ codenames-frontend/src/components/game/Puzzle.vue
@@ -1,5 +1,12 @@
 <template>
-    <div></div>
+    <div class="col-sm-12 row mx-0">
+        <div class="col-sm-6">
+            <p>{{puzzleWord}}</p>
+        </div>
+        <div class="col-sm-6">
+            <p>{{puzzleNumber}}</p>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
@@ -9,9 +16,24 @@
     @Component
     export default class Puzzle extends Vue {
 
+        get puzzleWord(): string {
+            return "puzzleWorld";
+        }
+
+        get puzzleNumber(): number {
+            return 8;
+        }
+
     }
 </script>
 
 <style scoped>
+    p {
+        font-size: 2rem;
+        color: rgb(135, 25, 75);
+        font-weight: bold;
+        text-shadow: 0 0 0.6rem white, 0px 0px 1.4rem rgb(255, 228, 22);
+        text-align: center;
+    }
 
 </style>
