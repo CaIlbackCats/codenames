002e7d5bd88021762aa800075992b8da267a8c88
Hudák Elza <hudak.elza@gmail.com>
2020-05-27 12:59:40 +0200

    fix button effect color

--- codenames-frontend/src/components/game/GameOptions.vue
+++ codenames-frontend/src/components/game/GameOptions.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="game-options">
-        <b-button squared v-if="!spyMaster">No Vote</b-button>
+        <b-button squared
+                  v-if="!spyMaster"
+        >No Vote
+        </b-button>
         <div class="col-sm-8 col-lg-6 offset-sm-2 offset-lg-3" v-else>
             <b-input required
                      size="md"
@@ -12,7 +15,10 @@
                         required
                         type="number"></b-form-input>
                 <b-input-group-append>
-                    <b-button squared type="submit">Ok</b-button>
+                    <b-button squared
+                              type="submit"
+                    >Ok
+                    </b-button>
                 </b-input-group-append>
             </b-input-group>
         </div>
@@ -40,7 +46,7 @@
         background-color: rgb(135, 25, 75);
         border: 0 solid;
         box-shadow: inset 0 0 20px rgba(250, 230, 15, 0);
-        outline: rgba(250, 230, 15, .5) solid 1px;
+        outline: rgba(135, 25, 75, .5) solid 1px;
         outline-offset: 0px;
         text-shadow: none;
         transition: all 1250ms cubic-bezier(0.19, 1, 0.22, 1);
