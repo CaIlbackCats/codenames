4ed5b397a05775a4cf25e55f22d1672eb71cef37
Hudák Elza <hudak.elza@gmail.com>
2020-05-31 22:47:38 +0200

    add stat fields

--- codenames-frontend/src/components/game/GameEnd.vue
+++ codenames-frontend/src/components/game/GameEnd.vue
@@ -3,15 +3,35 @@
         <div class="end-header">
             <img :class="{'move-down': moveLabel}" :src="logoUrl">
         </div>
-        <div class="white-backgrounded-div stats-div col-sm-12 col-lg-6 offset-3">
-            <p>Number of </p>
-            <p>civilians</p>
-            <p>enemy spy</p>
-            <p>rounds</p>
-            <p>passes</p>
-            <p>Best round</p>
-            <p>Worst round</p>
-            <p>Team accuracy</p>
+        <div class="white-backgrounded-div stats-div col-sm-12 col-lg-6 offset-3 text-left p-lg-4">
+            <span>Number of </span>
+            <hr>
+            <div class="row mx-0">
+                <div class="col-lg-5 offset-lg-1">
+                    <p>civilians</p>
+                    <p>enemy spies</p>
+                    <p>rounds</p>
+                    <p>passes</p>
+                </div>
+                <div class="col-lg-5 text-right">
+                    <p>0</p>
+                    <p>0</p>
+                    <p>0</p>
+                    <p>0</p>
+                </div>
+
+                <div class="col-lg-5 offset-lg-1 mt-lg-4">
+                    <p><span>Best round</span></p>
+                    <p><span>Worst round</span></p>
+                    <p><span>Team accuracy</span></p>
+                </div>
+                <div class="col-lg-5 text-right mt-lg-4">
+                    <p><span>0 spies in a row</span></p>
+                    <p><span>0 spies in a row</span></p>
+                    <p><span>0%</span></p>
+                </div>
+
+            </div>
         </div>
     </div>
 </template>
@@ -39,6 +59,20 @@
 </script>
 
 <style scoped>
+
+    span {
+        font-size: 1rem;
+        font-weight: bold;
+        color: rgb(135, 25, 75);
+        text-transform: uppercase;
+    }
+
+    p {
+        margin: 0;
+        color: rgba(135, 25, 75, 0.7);
+        font-size: 1.2rem;
+    }
+
     img {
         margin-top: -10rem;
         max-width: 60%;
